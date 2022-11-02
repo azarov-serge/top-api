fc702532a76a162d695d3e00abd627c99809338b
Azarov Serge <s.a.azarov@gmail.com>
2022-11-02 12:54:50 +0300

    db | Add mongo db + service for review

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PageModule } from './page/page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
+import { getMongoConfig } from './config/mongo.config';
 
 @Module({
-	imports: [AuthModule, PageModule, ProductModule, ReviewModule],
+	imports: [
+		ConfigModule.forRoot(),
+		TypegooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongoConfig
+		}),
+		AuthModule,
+		PageModule,
+		ProductModule,
+		ReviewModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,8 +1,24 @@
-export class ReviewModel {
-	id: string;
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import {Types} from 'mongoose';
+
+export interface ReviewModel extends Base {}
+export class ReviewModel extends TimeStamps {
+	@prop()
 	name: string;
+
+	@prop()
 	title: string;
+
+	@prop()
 	description: string;
- 	ating: number;
- 	reatedAt: Date;
+
+	@prop()
+	rating: number;
+
+	@prop()
+	createdAt: Date;
+
+	@prop()
+	productId: Types.ObjectId;
 }
